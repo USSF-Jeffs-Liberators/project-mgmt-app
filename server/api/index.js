08c36ec8108fda37b2704e50081bc825f1b2da1b
donnafarris <dlfarris97@gmail.com>
2020-12-29 15:40:13 -0500

    fixed issues with server side connections

--- server/api/index.js
+++ server/api/index.js
@@ -1,42 +1,57 @@
-const express = require('express')
-const bodyParser = require("body-parser")
-const app = express()
-const port = 3001
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3001;
+const cors = require("cors");
 
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: 'admin',
-    host: 'database',
-    database: 'project-mgmt',
-    password: 'admin',
-    port: 5432,
-})
+  user: "admin",
+  host: "database",
+  database: "project-mgmt",
+  password: "admin",
+  port: 5432,
+});
 
-app.use(bodyParser.json())
-app.use(cors())
-app.options('*', cors())
+app.use(bodyParser.json());
+app.use(cors());
+app.options("*", cors());
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client", err.stack);
+  }
+  client.query("SELECT NOW()", (err, result) => {
+    release();
+    if (err) {
+      return console.error("Error executing query", err.stack);
+    }
+    console.log(result.rows);
+  });
+});
 
 // users endpoints
-app.get('/users', (req, res) => {
-  pool.query('SELECT * FROM App_User', (error, results) => {
-      if (error) {
-          throw error;
-      }
-      res.status(200).json(results.rows)
-  })
-})
+app.get("/users", (req, res) => {
+  pool.query("SELECT * FROM App_User", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+});
 
 // project endpoints
-app.get('/projects', (req, res) => {
-  pool.query('SELECT * FROM Project', (error, results) => {
-      if (error) {
-          throw error;
-      }
-      res.status(200).json(results.rows)
-  })
-})
+app.get("/projects", (req, res) => {
+  pool.query("SELECT * FROM Project", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+});
 
 // other service endpoints...
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
