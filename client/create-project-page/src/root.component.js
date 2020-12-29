295d47b367226c3264ae186d02fae650ed231cbe
donnafarris <dlfarris97@gmail.com>
2020-12-29 14:34:26 -0500

    created create-project-page

---
+++ client/create-project-page/src/root.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { RuxTabs } from '@astrouxds/rux-tabs';
+import CreateProjectPage from "./a/create-project-page.component.js"
+
+export default function Root(props) {
+  return  (
+    <CreateProjectPage/>
+  );
+}
