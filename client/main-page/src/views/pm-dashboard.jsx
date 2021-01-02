87d27ea26b10b3a77826587b65e9a577633acbea
Emily Hosoya <emilyhosoya@gmail.com>
2021-01-02 12:44:38 -0700

    added sections to hold each component

--- client/main-page/src/views/pm-dashboard.jsx
+++ client/main-page/src/views/pm-dashboard.jsx
@@ -3,8 +3,13 @@ import React from "react";
 export default function ProjectManagerDashboard(props) {
 
   return (
-    <div id="projectManagerDashboard">
-        <h3>This is what a Project Manager would see after logging in.</h3>
+    <div class="dashboard">
+      <section class="project-timeline col-s-12 col-8"><h5>Gantt Chart</h5></section>
+      <section class="project-issues col-s-6 col-4"><h5>Issues Tracker</h5></section>
+      <section class="project-budget col-s-6 col-4"><h5>Budget Overview</h5></section>
+      <section class="project-team col-s-6 col-4"><h5>Team Members</h5></section>
+      <section class="project-requirements col-s-6 col-4"><h5>Project Requirements</h5></section>
+      <section class="project-tasks col-s-6 col-4"><h5>Task List</h5></section>
     </div>
   );
 }
