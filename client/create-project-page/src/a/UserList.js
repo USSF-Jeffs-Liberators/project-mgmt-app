e5fe3b40b4cbe19f663bd39b78871fcb135bf8c0
donnafarris <dlfarris97@gmail.com>
2021-01-04 12:35:06 -0500

    Create UserList.js

---
+++ client/create-project-page/src/a/UserList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function UserList(props) {
+    return (
+        <div>
+            <h3>Developers</h3>
+            <h3>Project Managers</h3>
+        </div>
+    )
+}
+
+export default UserList;
