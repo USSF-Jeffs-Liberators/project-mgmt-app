83fef163b8763b4d7e066267ff8e337496757378
donnafarris <dlfarris97@gmail.com>
2021-01-05 18:29:02 -0500

    Converted <ul> to <table>

--- client/create-project-page/src/a/UserList.jsx
+++ client/create-project-page/src/a/UserList.jsx
@@ -3,22 +3,56 @@ import React, { useState, useEffect } from "react";
 export default function UserList(props) {
   return (
     <div>
-      <h3>Developers</h3>
-      <ul>
-      {props.developers.map((developer) => (
-        <li key={developer.user_id}>
-          {developer.first_name} {developer.last_name}
-        </li>
-      ))}
-      </ul>
-      <h3>Project Managers</h3>
-      <ul>
-      {props.projectManagers.map((pm) => (
-        <li key={pm.user_id}>
-          {pm.first_name} {pm.last_name}
-        </li>
-      ))}
-      </ul>
+      <table className="rux-table">
+      <tbody>
+        <tr className="rux_table__column-head">
+          <th colSpan="6" align="center">Developers</th>
+        </tr>
+        <tr className="rux_table__column-head">
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Project ID</th>
+          <th>Project Name</th>
+          <th>Project Start Date</th>
+          <th>Project Deadline</th>
+        </tr>
+        {props.developers.map((each) => (
+          <tr key={each.user_id}>
+            <td>{each.first_name}</td>
+            <td>{each.last_name}</td>
+            <td>{each.project_id}</td>
+            <td>{each.project_name}</td>
+            <td>{each.start_date}</td>
+            <td>{each.deadline_date}</td>
+          </tr>
+        ))}
+      </tbody>
+      </table>
+      <table className="rux-table">
+      <tbody>
+        <tr className="rux_table__column-head">
+          <th colSpan="6" align="center">Project Managers</th>
+        </tr>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Project ID</th>
+          <th>Project Name</th>
+          <th>Project Start Date</th>
+          <th>Project Deadline</th>
+        </tr>
+        {props.projectManagers.map((each) => (
+          <tr key={each.user_id}>
+            <td>{each.first_name}</td>
+            <td>{each.last_name}</td>
+            <td>{each.project_id}</td>
+            <td>{each.project_name}</td>
+            <td>{each.start_date}</td>
+            <td>{each.deadline_date}</td>
+          </tr>
+        ))}
+        </tbody>
+        </table>
     </div>
   );
 }
