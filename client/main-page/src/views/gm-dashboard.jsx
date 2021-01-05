a5a14afa859506b83943f3a7a5929d77009da034
Emily Hosoya <emilyhosoya@gmail.com>
2021-01-05 14:30:33 -0700

    made UI changes to GM page

--- client/main-page/src/views/gm-dashboard.jsx
+++ client/main-page/src/views/gm-dashboard.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import GanttChart from "./gantt-chart/App";
+import ProjectRequirements from "./requirements/Requirements";
 
 export default function GeneralManagerDashboard(props) {
-
   return (
     <div class="dashboard">
-      <section class="project-timeline col-s-12 col-8"><h5>Selected Project Gantt Chart</h5></section>
-      <section class="gm-funding-requests col-s-6 col-4"><h5>All Open Funding Requests</h5></section>
-      <section class="gm-budget col-s-6 col-4"><h5>Budget Overview</h5></section>
-      <section class="gm-issues col-s-6 col-4"><h5>All Issues</h5></section>
-      <section class="gm-requirements col-s-6 col-4"><h5>All Requirements</h5></section>
-  </div>
+      <section class="project-timeline col-12">
+        <h5>Selected Project Gantt Chart</h5>
+        <GanttChart />
+      </section>
+      <section class="gm-funding-requests col-s-6 col-4">
+        <h5>All Open Funding Requests</h5>
+      </section>
+      <section class="gm-budget col-s-6 col-4">
+        <h5>Budget Overview</h5>
+      </section>
+      <section class="gm-issues col-s-6 col-4">
+        <h5>All Issues</h5>
+      </section>
+      <section class="gm-requirements col-s-6 col-4">
+        <h5>All Requirements</h5>
+        <ProjectRequirements />
+      </section>
+    </div>
   );
 }
