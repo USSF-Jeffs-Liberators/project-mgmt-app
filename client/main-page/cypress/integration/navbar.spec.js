6e2552506540a07fb3cafab062501045fa9e6fe6
Emily Hosoya <emilyhosoya@gmail.com>
2021-01-06 10:01:25 -0700

    resolved astro.svg error

--- client/main-page/cypress/integration/navbar.spec.js
+++ client/main-page/cypress/integration/navbar.spec.js
@@ -2,8 +2,10 @@ describe("Navbar", () => {
   beforeEach(() => {
       cy.visit('/')
   })
-  
   it("should contain our app name", () => {
     cy.get('nav').should('contain', 'Project Management App')
+  })
+  it("should contain Log In and Sign Up buttons when no one is logged in", () => {
+    cy.get('nav>rux-button-group').children().should('have.length', 2)
   })
 })
