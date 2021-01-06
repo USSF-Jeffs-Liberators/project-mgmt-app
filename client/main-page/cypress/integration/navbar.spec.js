cb57abde02a6e988a90f6845c56c4c8fdde1eedb
Emily Hosoya <emilyhosoya@gmail.com>
2021-01-06 12:20:29 -0700

    fixed navbar test

--- client/main-page/cypress/integration/navbar.spec.js
+++ client/main-page/cypress/integration/navbar.spec.js
@@ -6,6 +6,6 @@ describe("Navbar", () => {
     cy.get('nav').should('contain', 'Project Management App')
   })
   it("should contain Log In and Sign Up buttons when no one is logged in", () => {
-    cy.get('nav>rux-button-group').children().should('have.length', 2)
+    cy.get('nav').find('rux-button-group').children().should('have.length', 2)
   })
 })
