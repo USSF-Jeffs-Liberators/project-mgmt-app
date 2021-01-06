5cd740ebacc7672a59b35d56158d45c96b59bede
donnafarris <dlfarris97@gmail.com>
2021-01-06 12:10:37 -0500

    Form is rendering as expected

--- client/create-project-page/src/a/Form.jsx
+++ client/create-project-page/src/a/Form.jsx
@@ -4,7 +4,11 @@ function Form(props) {
   return (
     <div>
       <form onSubmit={props.onSubmit}>
-        <input type="submit" value="Submit" />
+        <div className="rux-form-field">
+          <label htmlFor="input_text">Text Input Label</label>
+          <input id="input__text" class="rux-input" type="text" required placeholder="Text Input"/>
+        </div>
+        <rux-button size="small"></rux-button>
       </form>
     </div>
   );
--- client/create-project-page/src/a/create-project-page.component.js
+++ client/create-project-page/src/a/create-project-page.component.js
@@ -9,14 +9,15 @@ class CreateProjectPage extends React.Component {
       developerList: [],
       pmList: [],
       gmList: [],
+      currentInput: {},
     };
-  };
+  }
 
   //fetches users by type, sets state
   async fetchUsers(type) {
-    var res = await fetch("http://localhost:3001/users/availability/"+type);
+    var res = await fetch("http://localhost:3001/users/availability/" + type);
     var json = await res.json();
-    switch(type) {
+    switch (type) {
       case "Developer":
         this.setState({
           ...this.state,
@@ -35,9 +36,8 @@ class CreateProjectPage extends React.Component {
       default:
         this.setState({
           ...this.state,
-        })        
+        });
     }
-    
   }
 
   async componentDidMount() {
@@ -46,16 +46,51 @@ class CreateProjectPage extends React.Component {
     this.fetchUsers("General Manager");
   }
 
+  handleChange(event) {
+    this.setState({currentInput: {[event.target.name]: event.target.value}});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.currentInput[0].value);
+    event.preventDefault();
+  }
+
+  // handleChange = (event) => {
+  //   if (!event.target.value){
+  //     return;
+  //   }
+  //   this.setState({ 
+  //     ...this.state,
+  //     [event.target.name]: event.target.value });
+  // };
+
+  // handleSubmit = (event) => {
+  //   alert("A form was submitted: " + this.state.event.target);
+
+  //   // fetch("https://your-node-server-here.com/api/endpoint", {
+  //   //   method: "POST",
+  //   //   // We convert the React state to JSON and send it as the POST body
+  //   //   body: JSON.stringify(this.state.event.target.value),
+  //   // }).then(function (response) {
+  //   //   console.log(response);
+  //   //   return response.json();
+  //   // });
+
+  //   event.preventDefault();
+  // };
+
   render() {
     return (
       <div>
-        <header/>
-        <UserList 
-        developers={this.state.developerList}
-        projectManagers={this.state.pmList}
-        generalManagers={this.state.gmList}
+        <header />
+        <UserList
+          developers={this.state.developerList}
+          projectManagers={this.state.pmList}
+          generalManagers={this.state.gmList}
         />
-        {/* <Form onSubmit={this.handleSubmit.bind(this)}/> */}
+        <Form
+          onChange={this.handleChange.bind(this)}
+          onSubmit={this.handleSubmit.bind(this)}/>
         <ul>
           <li>Be able to assign developers to work on a project.</li>
           <li>
