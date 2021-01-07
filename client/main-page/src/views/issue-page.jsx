2f8f09947fa45ab975fa734c053303840aa0c2b7
GooberShadow <benkehe@sbcglobal.net>
2021-01-07 16:16:57 -0600

    Created the html for submit issue page. Next step is integrating with my other issue.jsx file to allow the submit button to work

--- client/main-page/src/views/issue-page.jsx
+++ client/main-page/src/views/issue-page.jsx
@@ -7,31 +7,31 @@ export default function SubmitIssue(props) {
 
     return (
       <div id="loginpage">
-        <h4>Input information into various fields</h4>
+        <h4>Input the Project Id, a description of your issue, and the priority of the issue</h4>
   
         <form className="rux-form">
           <div className="rux-form-field">
-            <label htmlFor="input__text">Username</label>
+            <label htmlFor="input__text">Project Id</label>
+            <input id="input__text" className="rux-input" type="number" required />
+          </div>{" "}
+          <br />
+          <div className="rux-form-field">
+            <label htmlFor="input__text">Issue Description</label>
             <input id="input__text" className="rux-input" type="text" required />
           </div>{" "}
           <br />
           <div className="rux-form-field">
-            <label htmlFor="input__password">Password</label>
-            <input
-              id="input__password"
-              className="rux-input"
-              type="password"
-              required
-            />
+            <label htmlFor="input__text">Priority</label>
+            <select id="input__text" className="rux-input" type="text" required>
+              <option value = "low">Low</option>
+              <option value = "medium">Medium</option>
+              <option value = "high">High</option>
+            </select>
           </div>{" "}
           <br />
-          <rux-button type="button">Submit</rux-button>
+          <rux-button type="button">Submit Issue</rux-button>
         </form>
   
-        <h6>
-          Need an account? Sign up{" "}
-          <a className="signupLink" onClick={() => history.push("/issue")}>here</a>.
-        </h6>
       </div>
     );
   }
