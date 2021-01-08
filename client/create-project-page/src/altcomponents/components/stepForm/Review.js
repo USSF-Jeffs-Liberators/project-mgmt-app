84c32a6890208b6d6fc3762cf187cdb943e9cfee
donnafarris <dlfarris97@gmail.com>
2021-01-08 14:59:59 -0500

    as of now, it is working
    added multi-step form functionality, accordion functionality, continuing to flesh out accordion

---
+++ client/create-project-page/src/altcomponents/components/stepForm/Review.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { RuxAccordion } from "../Accordion/rux-accordion";
+
+export const Review = ({ formData }) => {
+  const {
+    firstName,
+    lastName,
+    nickName,
+    address,
+    city,
+    state,
+    zip,
+    phone,
+    email,
+  } = formData;
+
+  return (
+    <div>
+      <h1>Review</h1>
+      <RenderAccordion
+        summary="Names"
+        details={[
+          { 'First Name': firstName },
+          { 'Last Name': lastName },
+          { 'Nick Name': nickName },
+        ]}
+      />
+      <RenderAccordion
+        summary="Address"
+        details={[
+          { 'Address': address },
+          { 'City': city },
+          { 'State': state },
+          { 'Zip': zip },
+        ]}
+      />
+      <RenderAccordion
+        summary="Contact"
+        details={[
+            { 'Phone': phone }, 
+            { 'E-Mail': email }
+        ]}
+      />
+    </div>
+  );
+};
+
+export const RenderAccordion = ({ summary, details }) => (
+  <rux-accordion>
+    <span slot="label">{summary}</span>
+    <span slot="content">
+      <ul>
+        {details.map((data, index) => {
+          const objKey = Object.keys(data)[0];
+          const objValue = data[Object.keys(data)[0]];
+          return <li>{`${objKey}: ${objValue}`}</li>;
+        })}
+      </ul>
+      <button></button>
+    </span>
+  </rux-accordion>
+);
