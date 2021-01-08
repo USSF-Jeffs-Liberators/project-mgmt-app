b8d8f72ba4af9b44cd3faf6e013ed9eaa50ffe42
GooberShadow <benkehe@sbcglobal.net>
2021-01-08 13:56:59 -0600

    Developer Issues section finished. Merge to main

--- client/main-page/src/views/issue-page.js
+++ client/main-page/src/views/issue-page.js
@@ -12,33 +12,10 @@ class SubmitIssue extends React.Component{
       super(props)
 
       this.state = {
-        // projectIDBox: "",
-        // issueDescriptionBox: "",
-        // priorityBox: ""
       }
     }
 
-    // handleProjectIDBoxChange(event)
-    // {
-    //   this.setState({projectIDBox: event.target.value})
-    //   console.log("inside handleChangeProjectIDBox");
-    //   console.log(this.state.projectIDBox);
-    // }
-
-    // handleIssueDescriptionBoxChange(event)
-    // {
-    //   this.setState({issueDescriptionBox: event.target.value})
-    //   console.log("inside handleChangeIssueDescriptionBox");
-    //   console.log(this.state.issueDescriptionBox);
-    // }
-
-    // handlePriorityBoxChange(event)
-    // {
-    //   this.setState({priorityBox: event.target.value})
-    //   console.log("inside handleChangePriorityBox");
-    //   console.log(this.state.priorityBox);
-    // }
-
+    //Submit issue function
     async handleSubmitIssue(projectID, issueDesc, priority)
     {
       console.log(projectID)
@@ -78,36 +55,8 @@ class SubmitIssue extends React.Component{
     return (
       <div id="loginpage">
         <AddIssueComponent
-          // onProjectIDBoxChange={this.handleProjectIDBoxChange.bind(this)}
-          // onIssueDescriptionBoxChange={this.handleIssueDescriptionBoxChange.bind(this)}
-          // onPriorityBoxChange={this.handlePriorityBoxChange.bind(this)}
           onSubmitIssue={this.handleSubmitIssue.bind(this)}
         />
-        {/* <h4>Input the Project Id, a description of your issue, and the priority of the issue</h4>
-  
-        <form className="rux-form">
-          <div className="rux-form-field">
-            <label htmlFor="input__text">Project Id</label>
-            <input id="input__text" className="rux-input" type="number" required />
-          </div>{" "}
-          <br />
-          <div className="rux-form-field">
-            <label htmlFor="input__text">Issue Description</label>
-            <input id="input__text" className="rux-input" type="text" required />
-          </div>{" "}
-          <br />
-          <div className="rux-form-field">
-            <label htmlFor="input__text">Priority</label>
-            <select id="input__text" className="rux-input" type="text" required>
-              <option value = "low">Low</option>
-              <option value = "medium">Medium</option>
-              <option value = "high">High</option>
-            </select>
-          </div>{" "}
-          <br />
-          <rux-button type="button">Submit Issue</rux-button>
-        </form> */}
-  
       </div>
     );
   }
