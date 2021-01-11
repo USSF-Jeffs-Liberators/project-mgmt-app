61a4bcd8ebecd4d6ef2fc1ef3a107291d4dda1ec
Peter Vayda <petertvayda@gmail.com>
2021-01-11 13:04:20 -0800

    integrated user info with page render, navbar lags until new user logs in

--- client/main-page/src/root.component.js
+++ client/main-page/src/root.component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import LoginForm from "./views/login-page";
 import SignupForm from "./views/signup-page";
@@ -8,24 +8,50 @@ import ProjectManagerDashboard from "./views/pm-dashboard";
 import GeneralManagerDashboard from "./views/gm-dashboard";
 import SubmitIssue from "./views/issue-page";
 
+import AuthService from "./services/auth.service";
+import HomePage from "./views/HomePage";
+
 export default function Root(props) {
   // mock different User_Types
-  props = {
-    userType: "Developer",
-  };
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
+  // const user = AuthService.getCurrentUser();
+  // if(user){
+  //   props = {
+  //     userType: user.roles[0],
+  //     userId: user.user_id
+  //   };
+  // } else {
+  //   props = {
+  //     userType: "public",
+  //     userId: -1
+  //   };
+  // }
+  
 
   // "/" path will render a different page depending on user type
   const renderSwitch = () => {
-    switch (props.userType) {
-      case "Developer":
-        return <Route exact path="/" component={DeveloperDashboard} />;
-      case "Project Manager":
-        return <Route exact path="/" component={ProjectManagerDashboard} />;
-      case "General Manager":
-        return <Route exact path="/" component={GeneralManagerDashboard} />;
-      default:
-        return <Route exact path="/" component={LoggedOut} />;
+    if(currentUser){
+      switch (currentUser.roles[0]) {
+        case "Developer":
+          return <Route exact path="/" component={DeveloperDashboard} />;
+        case "Project Manager":
+          return <Route exact path="/" component={ProjectManagerDashboard} />;
+        case "General Manager":
+          return <Route exact path="/" component={GeneralManagerDashboard} />;
+        default:
+          return <Route exact path="/" component={HomePage} />;
+      }
+    } else {
+      return <Route exact path="/" component={HomePage} />;
     }
+    
   };
 
   return (
