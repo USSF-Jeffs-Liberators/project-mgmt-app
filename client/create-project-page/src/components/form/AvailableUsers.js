317a0fcf1567a4f4f24733925e89f87dc54f7761
donnafarris <dlfarris97@gmail.com>
2021-01-12 11:43:50 -0500

    refactored props

--- client/create-project-page/src/components/form/AvailableUsers.js
+++ client/create-project-page/src/components/form/AvailableUsers.js
@@ -1,44 +1,23 @@
 import React from "react";
 import { RuxAccordion } from "../../altcomponents/components/Accordion/rux-accordion";
 
-export const AvailableUsers = ({ navigation, userData, formData }) => {
-  const { go } = navigation;
-  const { projDeadline } = formData;
-  var formDeadlineDate = new Date(projDeadline);
-
-  function filterUsers(role) {
-    const roleFiltered = userData.filter((each) => each.name === role);
-    const dateFiltered = roleFiltered.filter((each) => {
-      var dataDeadline = new Date(each.deadline_date);
-      var dataEnd =
-        each.end_date != null // if the end date is not null
-          ? new Date(each.end_date) // set dataEnd to a new date using end_date
-          : new Date("2010-01-01"); // if end date IS null, set dataEnd to an arbitrary date that is before the current date
-      var dataStart = new Date(each.start_date);
-      return (
-        (dataDeadline < Date.now() &&
-          (each.end_date === null || dataEnd < Date.now())) || // if the user's listed projects ended before today // OR
-        dataStart > formDeadlineDate // the user's listed projects do not start until after the new project's deadline...
-      ); //the user will be visible for availability
-    });
-    return dateFiltered;
-  }
+export const AvailableUsers = (props) => {
 
   return (
     <div className="flex-child" style={{ marginLeft: "10px", minWidth: "950px", maxWidth:"950px" }}>
       <h1 style={{ marginTop: "26px" }}>Available Team Members</h1>
       <RenderAccordion
         summary="Project Managers"
-        availUsers={filterUsers("Project Manager")}
+        availUsers={props.filterUsers("Project Manager")}
       />
       <RenderAccordion
         summary="Developers"
-        availUsers={filterUsers("Developer")}
+        availUsers={props.filterUsers("Developer")}
       />
       <h1 style={{ marginTop: "26px" }}>All Team Members</h1>
       <RenderAccordion
         summary="All Developers And Project Managers"
-        availUsers={userData.filter((each) => each.first_name != 'admin')}
+        availUsers={props.userData.filter((each) => each.first_name != 'admin')}
       />
     </div>
   );
--- client/create-project-page/src/components/form/ProjTeam.js
+++ client/create-project-page/src/components/form/ProjTeam.js
@@ -3,12 +3,31 @@ import React from "react";
 import { AvailableUsers } from "./AvailableUsers";
 
 export const ProjTeam = ({ formData, userData, setForm, navigation }) => {
-  const { projStart, projDeadline } = formData;
-  const props = { navigation, userData, formData };
+  const props = { navigation, formData, userData };
+  const { projDeadline } = formData;
+  var formDeadlineDate = new Date(projDeadline);
+
+  function filterUsers(role) {
+    const roleFiltered = userData.filter((each) => each.name === role);
+    const dateFiltered = roleFiltered.filter((each) => {
+      var dataDeadline = new Date(each.deadline_date);
+      var dataEnd =
+        each.end_date != null // if the end date is not null
+          ? new Date(each.end_date) // set dataEnd to a new date using end_date
+          : new Date("2010-01-01"); // if end date IS null, set dataEnd to an arbitrary date that is before the current date
+      var dataStart = new Date(each.start_date);
+      return (
+        (dataDeadline < Date.now() &&
+          (each.end_date === null || dataEnd < Date.now())) || // if the user's listed projects ended before today // OR
+        dataStart > formDeadlineDate // the user's listed projects do not start until after the new project's deadline...
+      ); //the user will be visible for availability
+    });
+    return dateFiltered;
+  }
 
   return (
     <div className="flex-container">
-        <AvailableUsers {...props}/>
+        <AvailableUsers {...props} filterUsers={filterUsers}/>
       <form className="rux-form flex-child" style={{ minWidth: "225px" }}>
         <h1 style={{ marginTop: "26px" }}>Project Team</h1>
         <div className="rux-button-group">
