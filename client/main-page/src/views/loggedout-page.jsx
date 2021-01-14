bcb1479d00eccac70a978aef9e1448efc56b3dc4
Emily Hosoya <emilyhosoya@gmail.com>
2021-01-14 12:54:14 -0700

    added gif

--- client/main-page/src/views/loggedout-page.jsx
+++ client/main-page/src/views/loggedout-page.jsx
@@ -7,9 +7,9 @@ export default function LoggedOut(props) {
 
   return (
     <div id="loggedOutPage">
-      <h5>
-        You are not logged in.
-        <br />
+      <h3><img src="/static/img/animation.gif" alt="rocket blasting off"></img></h3>
+        <h3>You are not logged in.</h3>
+        <h5>
         <a className="loginLink" onClick={() => history.push("/login")}>Log in</a> or&nbsp;
         <a className="signupLink" onClick={() => history.push("/signup")}>Sign up</a>
       </h5>
